89bd23bfa60bd99a19c9a2d12502bb30eeb28b31
Ali <gharibali073@gmail.com>
2024-03-07 12:04:23 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Intranet Pharmacoop V3</title>
+        <title>Intranet Pharmacoop V6</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
